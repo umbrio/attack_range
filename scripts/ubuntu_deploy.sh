a4fd93b92dfa70235abeb13f2a6288251d87f55a
sonnypijl <61935995+sonnypijl@users.noreply.github.com>
2021-02-08 17:52:20 +0100

    Changed to variable paths

--- scripts/ubuntu_deploy.sh
+++ scripts/ubuntu_deploy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+## variables
+ATTACK_RANGE_URL="https://github.com/splunk/attack_range"
+ATTACK_RANGE_DIR="/opt/attack_range"
+TERRAFORM_BIN="/usr/local/bin/terraform"
+TERRAFORM_VERSION="0.14.4"
+
+## prerequisites
 sudo apt-get update
 sudo apt-get install -y python3-dev git unzip python3-pip awscli
 pip3 install virtualenv
-curl -s https://releases.hashicorp.com/terraform/0.14.4/terraform_0.14.4_linux_amd64.zip -o terraform.zip
-unzip terraform.zip
-sudo mv terraform /usr/local/bin/
-git clone https://github.com/splunk/attack_range && cd attack_range
-cd terraform/aws
+
+## install Terraform
+curl -s https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_$TERRAFORM_VERSION_linux_amd64.zip /
+     -o /tmp/terraform.zip
+unzip /tmp/terraform.zip
+sudo mv /tmp/terraform $TERRAFORM_BIN
+rm /tmp/terraform.zip
+
+## setup attack_range
+git clone $ATTACK_RANGE_URL $ATTACK_RANGE_DIR
+cd $ATTACK_RANGE_DIR
+cd $ATTACK_RANGE_DIR/terraform/aws
 terraform init
-cd ../..
-cd terraform/azure
+cd $ATTACK_RANGE_DIR/terraform/azure
 terraform init
-cd ../..
-virtualenv -p python3 venv
-source venv/bin/activate
-pip install -r requirements.txt
+virtualenv -p python3 $ATTACK_RANGE_DIR/venv
+source $ATTACK_RANGE_DIR/venv/bin/activate
+pip install -r $ATTACK_RANGE_DIR/requirements.txt
