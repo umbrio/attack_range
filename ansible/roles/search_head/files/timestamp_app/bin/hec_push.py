6c6bcca1e20feefa4678b2f46678f5054418b91b
sonnypijl <61935995+sonnypijl@users.noreply.github.com>
2021-03-12 14:15:44 +0100

    Update hec_push.py

--- ansible/roles/search_head/files/timestamp_app/bin/hec_push.py
+++ ansible/roles/search_head/files/timestamp_app/bin/hec_push.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python3
 from json import loads
+from datetime import datetime
 from splunk_http_event_collector import http_event_collector
+
+ 
 
 http_event_collector_key = "e84c9688-998c-4d9c-a715-64a0a87d1570"
 http_event_collector_host = "localhost"
 
+ 
+
 hec = http_event_collector(http_event_collector_key, http_event_collector_host)
 
+ 
+
 count = 0
+now = datetime.now().timestamp()
 with open('/tmp/heccollect.stash_hec', 'r') as collected:
     for event in collected:
         try:
-            hec.batchEvent(loads(event))
+            parsed = loads(event)
+            timestamp = float(parsed['time'])
+            days_diff = (now - timestamp)//86400
+            parsed['time'] = str(timestamp + (days_diff * 86400))
+            hec.batchEvent(parsed)
+
+ 
+
         except Exception as e:
             print(e)
+            continue
         else:
             count += 1
+
+ 
 
 hec.flushBatch()
 print(f"Imported {count} events")
